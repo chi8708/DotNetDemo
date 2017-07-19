520a20c4e992c86319311ca58d9638751e67d5eb
chi8708 <983143462@qq.com>
2017-07-19 10:52:20 +0800

    将多余项目移除

--- MvcApiHelpPage/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MvcApiHelpPage.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- MvcApiHelpPage/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MvcApiHelpPage.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
