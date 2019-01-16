827de801d55d5b8067af2dbc3902a380e43ca03b
chi8708 <983143462@qq.com>
2019-01-16 17:07:31 +0800

    将asyncMain包含

---
+++ BasicTest/asyncMain.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Threading;
+using System.Net.Http;
+
+namespace BasicTest
+{
+    /// <summary>
+    /// asynMain.xaml 的交互逻辑 
+    /// 源码https://docs.microsoft.com/zh-cn/dotnet/csharp/programming-guide/concepts/async/how-to-make-multiple-web-requests-in-parallel-by-using-async-and-await
+    /// </summary>
+    public partial class asynMain : Window
+    {
+        public asynMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            resultsTextBox.Clear();
+            await CreateMultipleTasksAsync();
+            resultsTextBox.Text += "\r\n\r\nControl returned to startButton_Click.\r\n";  
+        }
+
+        //await 分离以后， 3个task的执行完成顺序有变
+         private async Task CreateMultipleTasksAsync()  
+        {  
+            // Declare an HttpClient object, and increase the buffer size. The  
+            // default buffer size is 65,536.  
+            HttpClient client =  
+                new HttpClient() { MaxResponseContentBufferSize = 1000000 };  
+  
+            // Create and start the tasks. As each task finishes, DisplayResults   
+            // displays its length.  
+            Task<int> download1 =   
+                ProcessURLAsync("https://msdn.microsoft.com", client);  
+            Task<int> download2 =   
+                ProcessURLAsync("https://msdn.microsoft.com/library/hh156528(VS.110).aspx", client);  
+            Task<int> download3 =   
+                ProcessURLAsync("https://msdn.microsoft.com/library/67w7t67f.aspx", client);  
+  
+            // Await each task.  
+            int length1 = await download1;  
+            int length2 = await download2;  
+            int length3 = await download3;  
+  
+            int total = length1 + length2 + length3;  
+  
+            // Display the total count for the downloaded websites.  
+            resultsTextBox.Text += $"\r\n\r\nTotal bytes returned:  {total}\r\n";
+        }  
+  
+        async Task<int> ProcessURLAsync(string url, HttpClient client)  
+        {  
+            var byteArray = await client.GetByteArrayAsync(url);  
+            DisplayResults(url, byteArray);  
+            return byteArray.Length;  
+        }  
+  
+        private void DisplayResults(string url, byte[] content)  
+        {  
+            // Display the length of each website. The string format   
+            // is designed to be used with a monospaced font, such as  
+            // Lucida Console or Global Monospace.  
+            var bytes = content.Length;  
+            // Strip off the "https://".  
+            var displayURL = url.Replace("https://", "");  
+            resultsTextBox.Text += $"\n{displayURL,-58} {bytes,8}";
+        } 
+    }
+}
