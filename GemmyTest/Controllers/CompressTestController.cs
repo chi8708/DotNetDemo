7be9f4e56854b17fbab7c299ef51a1e26cc77409
chi8708 <983143462@qq.com>
2019-07-23 10:07:41 +0800

    compress压缩

---
+++ GemmyTest/Controllers/CompressTestController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GemmyTest.Controllers
+{
+    public class CompressTestController : Controller
+    {
+        // GET: CompressTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Compress()
+        {
+            var outFile = Server.MapPath("~/compress");
+            SevenZipCompressor.Zip(new List<string> { @"E:\compressTest" }, outFile);
+            return View();
+        }
+
+        [HttpPost]
+        public void Download(string o, string t)
+        {
+            string filePath = @"D:\Download\排期（20190412）.xlsx";
+
+            #region 处理下文件
+            var arr = filePath.Split('\\');
+            string fileName = arr[arr.Length - 1];
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(filePath);
+            if (fileInfo.Exists == true)
+            {
+                const long ChunkSize = 102400;//100K 每次读取文件，只读取100K，这样可以缓解服务器的压力
+                byte[] buffer = new byte[ChunkSize];
+
+                Response.Clear();
+                System.IO.FileStream iStream = System.IO.File.OpenRead(filePath);
+                long dataLengthToRead = iStream.Length;//获取下载的文件总大小
+                Response.ContentType = "application/octet-stream";
+                Response.AddHeader("Content-Disposition", "attachment; filename=" + HttpUtility.UrlEncode(fileName));
+                while (dataLengthToRead > 0 && Response.IsClientConnected)
+                {
+                    int lengthRead = iStream.Read(buffer, 0, Convert.ToInt32(ChunkSize));//读取的大小
+                    Response.OutputStream.Write(buffer, 0, lengthRead);
+                    Response.Flush();
+                    dataLengthToRead = dataLengthToRead - lengthRead;
+                }
+                Response.Close();
+                Response.End();
+            }
+            #endregion
+        }
+    }
+}
---
+++ GemmyTest/Controllers/EnterpriseServicesTestController.cs
@@ -0,0 +1,54 @@
+﻿using DBUtility;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.EnterpriseServices;
+using Dapper;
+
+namespace GemmyTest.Controllers
+{
+        
+    public class EnterpriseServicesTestController : Controller
+    {
+     
+        // GET: /EnterpriseServicesTest/
+        public ActionResult Index()
+        {
+            new AddService().AddTran();
+            return View();
+        }
+
+      
+	}
+
+    [System.Runtime.InteropServices.ComVisible(true)]
+    [Transaction(TransactionOption.Required)]
+    public class AddService : ServicedComponent 
+    {
+        private static readonly string connStr = PubConstant.ConnectionString;
+        IDbConnection conn = new SqlConnection(connStr);
+
+        public AddService() { }
+
+        [AutoComplete]
+        public void AddTran() 
+        {
+            InsertTestTable1();
+            var a = 0;
+            var b = 1/a;
+        }
+        
+       
+        public void InsertTestTable1()
+        {
+            var sql = @"INSERT INTO [Table1]([name]) VALUES (@a)";
+            sql = string.Format(sql, new[] { new { a = "es1" }, new { a = "es2" } });
+
+            conn.Execute(sql);
+        }
+    }
+}
